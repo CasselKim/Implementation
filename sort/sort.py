f2f095148ef808a8d0bc712843b90ec730c9a9f5
CasselKim <seongr39@gmail.com>
2022-07-09 23:33:29 +0900

    increase test case size

--- sort/sort.py
+++ sort/sort.py
@@ -10,10 +10,10 @@ def isSorted(array) :
     return True
     
 def createTestCases() : 
-    array_normal = [random.randint(1,1000) for _ in range(1000)]
-    array_best = [i for i in range(1000)]
-    array_worst = [i for i in range(1000,-1,-1)]
-    return [array_normal, array_best, array_worst]
+    array_normal = [random.randint(1,100000) for _ in range(100000)]
+    array_best = [i for i in range(100000)]
+    array_worst = [i for i in range(100000,-1,-1)]
+    return {"array_normal":array_normal, "array_best":array_best, "array_worst":array_worst}
 
 def checkTimeFrom() : 
     return time.time()
@@ -25,7 +25,10 @@ if __name__ == "__main__" :
 
     test_cases = createTestCases()
 
-    for case in test_cases : 
+    for case_type,case in test_cases.items() : 
+
+        print("case :",case_type)
+        
         # insertion sort
         start = checkTimeFrom()
         assert( isSorted(insertionSort(case)) )
@@ -33,3 +36,5 @@ if __name__ == "__main__" :
         start = checkTimeFrom()
         assert( isSorted(insertionSortBinarySearch(case)) )
         checkTimeTo(start,"insertionSortBinarySearch")
+
+        print()
