60705e9e163cd9c4d05256493feb8ff689c40ada
CasselKim <seongr39@gmail.com>
2022-07-09 23:26:59 +0900

    add conducting time estimation

--- sort/sort.py
+++ sort/sort.py
@@ -1,6 +1,7 @@
 from typing import List
 from insertionSort import insertionSort, insertionSortBinarySearch
 import random
+import time
 
 def isSorted(array) :
     for i in range(1,len(array)) : 
@@ -14,11 +15,21 @@ def createTestCases() :
     array_worst = [i for i in range(1000,-1,-1)]
     return [array_normal, array_best, array_worst]
 
+def checkTimeFrom() : 
+    return time.time()
+
+def checkTimeTo(start,func_name) : 
+    print("{} conducting time : {}".format(func_name,time.time()-start))
+
 if __name__ == "__main__" :
 
     test_cases = createTestCases()
 
     for case in test_cases : 
         # insertion sort
+        start = checkTimeFrom()
         assert( isSorted(insertionSort(case)) )
+        checkTimeTo(start,"insertionSort")
+        start = checkTimeFrom()
         assert( isSorted(insertionSortBinarySearch(case)) )
+        checkTimeTo(start,"insertionSortBinarySearch")
