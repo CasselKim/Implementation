bca3fd72f3f68a0daa3bf4ab731715367900c949
CasselKim <seongr39@gmail.com>
2022-07-12 23:22:54 +0900

    add bubble sort method

---
+++ sort/bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(array) : 
+    swap = True
+    while swap : 
+        swap = False
+        for i in range(len(array)-1) : 
+            if array[i]>array[i+1] : 
+                array[i+1], array[i] = array[i], array[i+1]
+                swap=True
+    return array
