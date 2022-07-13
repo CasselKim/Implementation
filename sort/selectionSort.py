7b4a7f839bd4eff5eb9e06b34fa0d18be8430465
CasselKim <seongr39@gmail.com>
2022-07-13 21:21:18 +0900

    add files : selectionSort.py

---
+++ sort/selectionSort.py
@@ -0,0 +1,8 @@
+def selectionSort(arr) : 
+    for i in range(len(arr)) : 
+        minimum = (arr[i], i)
+        for j in range(i,len(arr)) : 
+            if arr[j]<minimum[0] : 
+                minimum=(arr[j],j)
+        arr[i], arr[minimum[1]] = arr[minimum[1]], arr[i]
+    return arr
